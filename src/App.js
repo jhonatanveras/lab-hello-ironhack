25775bc5d837dfafeefd69fe0eba1bbf4a6b3061
Jhonatan Veras <jveras@leroymerlin.com.br>
2021-07-13 18:39:46 -0300

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import Header from "./Header";
+import Elements from "./Elements";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Elements/>
+      </div>
+    )
+  }
+}
+
+export default App;
